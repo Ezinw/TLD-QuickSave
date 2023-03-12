e75890cba44ae8db712cf4d2ac5f590eddb5b2a3
Ezinw <112014367+Ezinw@users.noreply.github.com>
2023-03-12 15:33:22 -0300

    Create Patches.cs

---
+++ src/Patches.cs
@@ -0,0 +1,41 @@
+using HarmonyLib;
+using Il2Cpp;
+using UnityEngine;
+
+namespace QuickSave
+{
+    [HarmonyPatch(typeof(GameManager), "Update")]
+    internal class QuickSave
+    {
+        private static readonly KeyCode saveKey = KeyCode.F5;
+
+        static void Postfix(GameManager __instance)
+        {
+            if (Input.GetKeyDown(saveKey))
+            {
+                GameManager.SaveGameAndDisplayHUDMessage();
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(GameAudioManager), "PlayGUIError")]
+    class GameAudioManager_PlayGUIError
+    {
+        static bool Prefix(GameAudioManager __instance)
+        {
+            if (Input.GetKeyDown(KeyCode.F5))
+            {
+                AudioSource audioSource = __instance.gameObject.GetComponent<AudioSource>();
+
+                if (audioSource.clip != null && audioSource.clip.name == "PlayGUIError")
+                {
+                    audioSource.Stop();
+
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
