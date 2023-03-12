cae55ef1a88c4f0555db23aaca0d1e8d5baceb2f
Ezinw <112014367+Ezinw@users.noreply.github.com>
2023-03-12 15:05:29 -0300

    Create Implementation.cs

---
+++ src/Implementation.cs
@@ -0,0 +1,12 @@
+using MelonLoader;
+
+namespace QuickSave
+{
+    internal sealed class Implementation : MelonMod
+    {
+        public override void OnInitializeMelon()
+        {
+
+        }
+    }
+}
